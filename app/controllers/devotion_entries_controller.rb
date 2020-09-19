bdf6588f0ffc4c6703aaa0006ec82c6ac54b1eeb
joshsrai <joshsrai@gmail.com>
2020-09-19 18:24:21 -0400

    20200919182409 devoentcont

--- app/controllers/devotion_entries_controller.rb
+++ app/controllers/devotion_entries_controller.rb
@@ -4,6 +4,20 @@ class DevotionEntriesController < ApplicationController
         erb :'/devotion_entries/new'
     end
 
+    post '/devotion_entries' do
+
+        if !logged_in?
+            redirect '/'
+        end
+        if params[:content] != ""
+            @devotion_entry = DevotionEntry.create(content: params[:content], user_id: current_user.id)
+            redirect "/devotion_entries/#{@devotion_entry.id}"
+        else
+            redirect '/devotion_entries/new'
+
+        end
+    end
+
 
     
 end
