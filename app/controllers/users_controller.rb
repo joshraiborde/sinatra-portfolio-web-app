8234bbdbf8aec4e44141f4c12ae5faee4d4d8d7c
joshsrai <joshsrai@gmail.com>
2020-09-19 14:33:42 -0400

    20200919143305 sessions, get post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,27 @@ class UsersController < ApplicationController
         erb :login
     end
 
+    #login form
+    post '/login' do
+        @user = User.find_by(email: params[:email])
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            puts session
+            redirect "users/#{@user.id}"
+
+        else
+
+        end
+        
+    end
+
     get 'signup' do
         
+    end
+
+    #show route
+    get '/users/:id' do
+        "show route"
     end
 
 
