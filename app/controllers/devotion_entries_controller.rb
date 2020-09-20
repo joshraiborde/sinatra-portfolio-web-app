f4f41fe86933485fb7a6f5767da203e9c17ab900
joshsrai <joshsrai@gmail.com>
2020-09-19 22:08:58 -0400

    20200919200832 deventcon,viewdeventedit

--- app/controllers/devotion_entries_controller.rb
+++ app/controllers/devotion_entries_controller.rb
@@ -14,19 +14,47 @@ class DevotionEntriesController < ApplicationController
             redirect "/devotion_entries/#{@devotion_entry.id}"
         else
             redirect '/devotion_entries/new'
-
         end
     end
 
     get '/devotion_entries/:id' do
-        @devotion_entry = DevotionEntry.find(params[:id])
+        set_devotion_entry
         erb :'/devotion_entries/show'
     end
 
     #to edit devotion entries
-    get '/devotion_entries/10/edit' do
-        erb :'devotion_entries/edit'
+    get '/devotion_entries/:id/edit' do
+        set_devotion_entry
+        if logged_in?
+            if @devotion_entry.user == current_user 
+                erb :'/devotion_entries/edit'
+            else
+                redirect "user/#{current_user.id}"
+            end
+        else
+            redirect '/'
+        end
+    end
+
+    #patch
+    patch '/devotion_entries/:id' do
+        set_devotion_entry
+        if logged_in?
+            if @devotion_entry.user == current_user
+                @devotion_entry.update(content: params[:content])
+                redirect "/devotion_entries/#{@devotion_entry.id}"
+            else
+                redirect "users/#{current_user.id}"
+            end
+        else
+            redirect '/'
+        end        
     end
     
+    private
+
+    def set_devotion_entry
+        @devotion_entry = DevotionEntry.find(params[:id])
+    end
     
 end
